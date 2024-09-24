3318be39cfcf6d2447005934c2dd03dc8c618d20
OSobol <osobol@theglobal.net>
2024-09-24 09:45:41 -0600

    Temporary changed port to 5060

--- api_callback.py
+++ api_callback.py
@@ -239,7 +239,7 @@ class UtopiaAPIHandler:
         }
 
     def run(self):
-        self.app.run(host='0.0.0.0', port=5050)
+        self.app.run(host='0.0.0.0', port=5060)
 
 
 if __name__ == "__main__":
